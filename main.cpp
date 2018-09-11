9ba563dcd26116b23e33b47135637a1789268f5b
brendanVW <36153110+brendanVW@users.noreply.github.com>
2018-09-11 09:40:50 +0200

    Changed name (#13)

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@ using namespace std;
 
 int main() 
 {
+    cout << "Brendan van Walstijn, 500733099";
     cout << "Albert van Kiel, 500729041";
-
     cout << "Ramon Gill, 500733062"
-
     return 0;
 }
